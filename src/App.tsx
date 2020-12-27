158236ad13ee519362dd5a8fe58e18b49cd6f586
a916856595 <hcj147@vip.qq.com>
2020-12-27 19:42:20 +0800

    refactor: 修改ts部分配置、添加简单示例

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,87 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import './App.scss';
+import FullScroll from './reactFullScroll/full-scroll';
 
-function App() {
-  return (
-    <div className="App">
+class App extends Component<any, any> {
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeKey: 'first'
+    }
+  }
 
-    </div>
-  );
+  render() {
+    const { activeKey } = this.state;
+
+    return (
+        <div className="home-page">
+          <FullScroll
+              activeKey={activeKey}
+              addEventToDocument
+          >
+            <FullScroll.Item
+                key="first"
+            >
+              <div className="first-screen screen">
+                <p>第一屏</p>
+                <p>第一屏</p>
+                <p>第一屏</p>
+                <p>第一屏</p>
+                <p>第一屏</p>
+                <p>第一屏</p>
+                <p>第一屏</p>
+              </div>
+            </FullScroll.Item>
+            <FullScroll.Item
+                key="second"
+            >
+              <div className="second-screen screen">
+                <p>第二屏</p>
+                <p>第二屏</p>
+                <p>第二屏</p>
+                <p>第二屏</p>
+                <p>第二屏</p>
+                <p>第二屏</p>
+                <p>第二屏</p>
+                <p>第二屏</p>
+                <p>第二屏</p>
+              </div>
+            </FullScroll.Item>
+            <FullScroll.Item
+                key="third"
+            >
+              <div className="third-screen screen">
+                <p>第三屏</p>
+                <p>第三屏</p>
+                <p>第三屏</p>
+                <p>第三屏</p>
+                <p>第三屏</p>
+                <p>第三屏</p>
+                <p>第三屏</p>
+                <p>第三屏</p>
+              </div>
+            </FullScroll.Item>
+            <FullScroll.Item
+                key="fourth"
+            >
+              <div className="fourth-screen screen">
+                <p>第四屏</p>
+                <p>第四屏</p>
+                <p>第四屏</p>
+                <p>第四屏</p>
+                <p>第四屏</p>
+                <p>第四屏</p>
+                <p>第四屏</p>
+                <p>第四屏</p>
+                <p>第四屏</p>
+                <p>第四屏</p>
+                <p>第四屏</p>
+              </div>
+            </FullScroll.Item>
+          </FullScroll>
+        </div>
+    );
+  }
 }
 
 export default App;
